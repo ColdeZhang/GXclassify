388992470d718a4cdeefec1b1aea90530d2a80bb
ColdeZhang <coldezhang@foxmail.com>
2020-10-02 11:13:54 +0800

    Update chart.py

--- chart.py
+++ chart.py
@@ -5,15 +5,11 @@ from pyecharts.globals import SymbolType
 c1 = (
     Liquid()
     .add("lq", [0.3, 0.7], is_outline_show=False, shape=SymbolType.DIAMOND)
-    .set_global_opts(title_opts=opts.TitleOpts(title="Liquid-Shape-Diamond"))
-    .render("liquid_shape_diamond")
 )
 
 c2 = (
     Liquid()
     .add("lq", [0.3, 0.7], is_outline_show=False, shape=SymbolType.DIAMOND)
-    .set_global_opts(title_opts=opts.TitleOpts(title="Liquid-Shape-Diamond"))
-    .render("liquid_shape_diamond")
 )
 
 grid = Grid().add(c1, grid_opts=opts.GridOpts()).add(c2, grid_opts=opts.GridOpts())
