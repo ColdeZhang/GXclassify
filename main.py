5b81713f31586840b9644ce60193219619d2e1dd
ColdeZhang <coldezhang@foxmail.com>
2020-10-02 10:18:17 +0800

    .

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import UIExecutor as UI
 brow = UI.init()
 time.sleep(1)
 UI.videoPage(brow)
-time.sleep(1)
+time.sleep(10)
 UI.waitingPage(brow, '测试中……')
 time.sleep(4)
 UI.outputPage(brow, '某垃圾', '有害垃圾')
