bceebe33d4fa626d546d2d4b3ccd0a46e08c906b
ColdeZhang <coldezhang@foxmail.com>
2020-10-02 13:33:04 +0800

    .

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import extension
 import RPi.GPIO as GPIO
 import UIExecutor as UI
 
+devInfo = ['16','7','32','6','78','15','2','4']
 
 brow = UI.init()
 time.sleep(1)
@@ -12,3 +13,5 @@ time.sleep(100)
 UI.waitingPage(brow, '测试中……')
 time.sleep(4)
 UI.outputPage(brow, '某垃圾', '有害垃圾')
+time.sleep(6)
+UI.devInfoPage(brow,devInfo)
